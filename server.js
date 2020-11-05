b70b84b9c7fd578b69812339ee0adb3a0285c3ec
javisolercanto <jsolercanto11@gmail.com>
2020-11-05 16:17:12 +0100

    whatsapp mode

--- server.js
+++ server.js
@@ -12,13 +12,25 @@ app.get('/', (req, res) => {
 
 io.on('connect', (socket) => {
     socket.on('hello', data => {
-        console.log('hola', data);
+        console.log(data);
     });
     socket.on('newMessage', data => {
-        console.log('new', data);
-        socket.emit('chat message', data)
+        console.log('msg', data);
+        io.to(data.room.name).emit('chat message', data);
     });
+    socket.on('join', data => {
+        console.log('join', data);
+        socket.join(data.room.name);
+        io.to(data.room.name).emit('joined player', data);
+        
+        /* socket.emit('chat message', data) */
 
+        /**
+         {id: xxx, players: [], counter: zzz}
+         */
+
+        /* socket.to().emit(); */
+    });
 });
 
 http.listen(666, () => {
